524aea31c6305d0fe2152d1385b2fcdc66cde122
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-03-19 18:55:21 +0100

    Fix various file path/name errors + sql formatting issue in image route

--- back-end/src/routes/images/index.ts
+++ back-end/src/routes/images/index.ts
@@ -1,15 +1,16 @@
 import { Request, Router } from 'express'
 import multer from 'multer'
+import path from 'path'
 import format from 'pg-format'
 import { pool } from '../../helpers/pg-pool'
-import { RESOURCE_NOT_FOUND } from '../../helpers/status-codes'
+import { BAD_REQUEST, RESOURCE_NOT_FOUND } from '../../helpers/status-codes'
 
 const storage = multer.diskStorage({
     destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
-        cb(null, '../../../imgs')
+        cb(null, 'imgs/')
     },
     filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-        const fileName = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const fileName = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.webp'
         cb(null, fileName)
     }
 })
@@ -19,9 +20,9 @@ const upload = multer({ storage })
 const ImageRouter = Router()
 
 ImageRouter.post('/', upload.array('images', 10), (req, res) => {
-    if (req.files === undefined) res.status(204).send()
+    if (req.files === undefined || req.files.length === 0) res.status(400).send(BAD_REQUEST)
     else {
-        const sql = format('INSERT INTO picture (picure_path) VALUES %L RETURNING id', (<Express.Multer.File[]>req.files).map(file => file.filename))
+        const sql = format('INSERT INTO picture (picture_path) VALUES %L RETURNING id', (<Express.Multer.File[]>req.files).map(file => [file.filename.slice(0, -5)]))
 
         pool.query(sql)
             .then(result => res.status(201).send(result.rows))
@@ -34,7 +35,7 @@ ImageRouter.get('/:id(\\d+)', (req, res) => {
     pool.query(sql, [req.params.id])
         .then(result => {
             if (result.rowCount === 0) res.status(404).send(RESOURCE_NOT_FOUND)
-            else res.status(200).sendFile('../../../imgs/' + result.rows[0].picture_path)
+            else res.status(200).sendFile(path.resolve('imgs/' + result.rows[0].picturePath + '.webp'))
         })
 })
 

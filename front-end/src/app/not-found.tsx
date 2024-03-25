4f9c6552fd1d4233c6c88c394030d281332ede40
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-03-25 14:28:40 +0100

    Create custom 404 page

---
+++ front-end/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center gap-6">
+            <p className="text-4xl font-semibold text-center">Nothing to see here...</p>
+            <p className="text-gray-500 text-center">404 | Not found</p>
+        </div>
+    )
+}

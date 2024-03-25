ff43a83e97a7d9209250ab380449d43d4207a1df
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-03-25 21:38:16 +0100

    Prevent drag enter events from spreading to parents (for when it is rendered inside an image uploader)

--- front-end/src/components/blur-overlay/index.tsx
+++ front-end/src/components/blur-overlay/index.tsx
@@ -1,3 +1,5 @@
 export default function BlurOverlay() {
-    return <div className='fixed top-0 right-0 w-screen h-screen backdrop-blur-sm z-10 bg-black/40'/>
+    const stopEvent = (e: React.BaseSyntheticEvent) => e.stopPropagation()
+
+    return <div onDragOver={stopEvent} onDragEnter={stopEvent} className='fixed top-0 right-0 w-screen h-screen backdrop-blur-sm z-10 bg-black/40'/>
 }

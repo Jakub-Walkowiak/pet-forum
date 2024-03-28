b40cb5d4dc3b9a0f49802fd4af40c3bcc6c5f594
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-03-28 19:46:46 +0100

    Correct absolute positioning of notification popup to fixed

--- front-end/src/components/notification-popup/index.tsx
+++ front-end/src/components/notification-popup/index.tsx
@@ -7,7 +7,7 @@ interface NotificationPopupProps {
 
 export default function NotificationPopup({ positive = false, text }: NotificationPopupProps) {
     return (
-        <div className={`${positive ? 'bg-emerald-600' : 'bg-red-600'} z-50 flex justify-center items-center gap-2 absolute inset-x-0 m-auto bottom-8 w-72 h-12 font-medium text-md rounded-lg animate-fade-out animate-poop opacity-1`}>
+        <div className={`${positive ? 'bg-emerald-600' : 'bg-red-600'} z-50 flex justify-center items-center gap-2 fixed inset-x-0 m-auto bottom-8 w-72 h-12 font-medium text-md rounded-lg animate-fade-out animate-poop opacity-1`}>
             {positive ? <AiFillCheckCircle className="text-2xl"/> : <AiFillExclamationCircle  className="text-2xl"/>} {text}
         </div>
     )

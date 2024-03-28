6524db70474ff9f074de0c6d3ce7a8a3a03b6284
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-03-28 19:43:36 +0100

    Add tag hook

---
+++ front-end/src/hooks/use-tag.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export interface TagData {
+    tagName: string,
+    timesUsed: number,
+}
+
+export default function useTag(id: number, advice: boolean = false) {
+    const [tagData, setTagData] = useState<TagData | undefined>()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:3000/${advice ? 'advice-posts' : 'blog-posts'}/tags/${id}`)
+            if (!response.ok) setTagData(undefined)
+            else setTagData(await response.json())
+        }
+        try { fetchData() } catch(err) { setTagData(undefined) }
+    }, [id, advice])
+
+    return tagData
+}

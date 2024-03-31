1abe995ac8681bd69aedf01726ea4e6f5ac3e3ed
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-03-31 17:59:12 +0200

    Remove sticky positioning from homepage post panel

--- front-end/src/components/new-post-panel/index.tsx
+++ front-end/src/components/new-post-panel/index.tsx
@@ -7,7 +7,7 @@ export default function NewPostPanel() {
     const auth = useAuth()
 
     return (
-        <div className={`${auth === undefined && 'hidden'} w-full min-h-16 sticky border-b border-zinc-700 flex items-center justify-center p-4`}>
+        <div className={`${auth === undefined && 'hidden'} w-full min-h-16 border-b border-zinc-700 flex items-center justify-center p-4`}>
             <PostCreator maxRows={8} placeholder="What do you want to share?"/>
         </div>
     )

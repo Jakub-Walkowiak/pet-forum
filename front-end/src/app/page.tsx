c5b9a0fbd417da3c8c23781785fb5d1893a72ba6
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-02 19:24:30 +0200

    Rework home page

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,10 +1,20 @@
+'use client'
+
+import BlogFeed from "@/components/blog-feed";
 import NewPostPanel from "@/components/new-post-panel";
+import TabContainer from "@/components/tab-container";
+import useAuth from "@/hooks/use-auth";
 
 
 export default function Home() {
+    const auth = useAuth()
+
     return (
         <>
-            <NewPostPanel/>
+            {auth ? <TabContainer header={<NewPostPanel/>} tabs={[
+                { title: 'Discover', element: <BlogFeed options={{ replies: false, orderBy: 'like_count' }}/> },
+                { title: 'Followed', element: <BlogFeed options={{ desiredUsers: 'followed', orderBy: 'date_posted' }}/> },
+            ]}/> : <BlogFeed options={{ replies: false, orderBy: 'like_count' }}/>}
         </>
     )
 }

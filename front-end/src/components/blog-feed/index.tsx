dddfe6a9ba6b6a25143469d8f1dc102dea67a01e
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-02 16:16:30 +0200

    Add reusable blog post feed component

--- front-end/src/components/blog-feed/index.tsx
+++ front-end/src/components/blog-feed/index.tsx
@@ -1,25 +1,43 @@
 'use client'
 
+import getBlogPosts, { BlogPostFetchOptions } from "@/helpers/get-posts";
 import { useEffect, useState } from "react";
-import BlogPost from "../blog-post";
+import { AiOutlineLoading } from "react-icons/ai";
+import BlogPostGroup from "../blog-post-group";
 
-export default function BlogFeed() {
+interface BlogFeedProps {
+    options?: BlogPostFetchOptions,
+}
+
+export default function BlogFeed({ options }: BlogFeedProps) {
+    const [postGenerator] = useState(getBlogPosts(options))
+    const [loading, setLoading] = useState(false)
     const [posts, setPosts] = useState(new Array<number>())
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch('http://localhost:3000/blog-posts')
-            setPosts(((await response.json()) as { id: number }[]).map(row => row.id))
-        }
+    const fetchPosts = async () => {
+        const newPosts = (await postGenerator.next()).value
+        if (Array.isArray(newPosts)) setPosts(old => [...old, ...newPosts])
+    }
 
-        fetchPosts()
+    useEffect(() => { 
+        fetchPosts() 
+        window.addEventListener('scroll', async (e) => {
+            if (document.documentElement.scrollHeight - document.documentElement.clientHeight - document.documentElement.scrollTop < 1) {
+                setLoading(true)
+                await fetchPosts()
+                setLoading(false)
+            }
+        })
     }, [])
 
     return (
         <ul className="w-full flex flex-col list-none divide-y divide-zinc-700">
             {posts.map(id => (
-                <li key={id}><BlogPost postId={id}/></li>
+                <li key={id}><BlogPostGroup startId={id}/></li>
             ))}
+            {loading 
+                ? <li className="w-full flex items-center justify-center h-32 mt-1 text-5xl"><AiOutlineLoading className="animate-spin"/></li>
+                : <li className="w-full flex items-center justify-center h-32 mt-1 text-xl text-gray-500">Nothing too see here...</li>}
         </ul>
     )
 }
---
+++ front-end/src/helpers/get-posts.ts
@@ -0,0 +1,46 @@
+export interface BlogPostFetchOptions {
+    orderBy?: 'like_count' | 'date_posted' | 'reply_count',
+    orderMode?: 'ASC | DESC',
+    fromUser?: number,
+    replies?: boolean,
+    replyTo?: number,
+    desiredUsers?: 'followed' | 'mutuals' | 'none',
+    tags?: Array<number>,
+    tagMode?: 'any' | 'all',
+    limit?: number,
+    offset?: number,
+    contains?: string,
+    pets?: Array<number>,
+    petMode?: 'any' | 'all',
+}
+
+export default async function* getBlogPosts(options?: BlogPostFetchOptions) {
+    let offset = options?.offset === undefined ? 0 : options.offset
+    const queryBody = options === undefined 
+        ? `limit=10` 
+        : [options.limit ? `limit=${options.limit}` : 'limit=10',
+            options.orderBy ? `orderBy=${options.orderBy}` : '',
+            options.orderMode ? `orderMode=${options.orderMode}` : '',
+            options.fromUser ? `fromUser=${options.fromUser}` : '', 
+            options.replies ? `replies=${options.replies}` : '', 
+            options.replyTo ? `replyTo=${options.replyTo}` : '', 
+            options.desiredUsers ? `desiredUsers=${options.desiredUsers}` : '', 
+            options.tagMode ? `tagMode=${options.tagMode}` : '', 
+            options.contains ? `contains=${options.contains}` : '', 
+            options.petMode ? `petMode=${options.petMode}` : '', 
+            options.tags && options.tags.length > 0 ? options.tags.map(tag => `tags[]=${tag}`).join('&') : '',
+            options.pets && options.pets.length > 0 ? options.pets.map(pet => `pets[]=${pet}`).join('&') : '',
+        ].filter(str => str !== '').join('&')
+
+    while (true) {
+        const response = await fetch(`http://localhost:3000/blog-posts?offset=${offset}&` +  queryBody)
+
+        if (response.ok) {
+            const json = (await response.json()) as { id: number }[]
+            if (json.length > 0) {
+                yield json.map(row => Number(row.id))
+                offset += options?.limit !== undefined ? options?.limit : 10
+            } else yield undefined
+        } else yield undefined
+    }
+}

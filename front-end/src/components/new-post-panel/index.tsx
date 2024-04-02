ee77a5ae803fe9fdae50cd77de86dbf2bae8a348
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-02 19:23:55 +0200

    Remove check for authorization, will instead be handled externally

--- front-end/src/components/new-post-panel/index.tsx
+++ front-end/src/components/new-post-panel/index.tsx
@@ -1,17 +1,15 @@
 'use client'
 
-import useAuth from "@/hooks/use-auth";
 import { useState } from "react";
 import BlogPostGroup from "../blog-post-group";
 import PostCreator from "../form-utils/post-creator";
 
 export default function NewPostPanel() {
-    const auth = useAuth()
     const [created, setCreated] = useState(new Array<number>())
 
     return (
         <>
-            <div className={`${auth === undefined && 'hidden'} w-full min-h-16 border-b border-zinc-700 flex items-center justify-center p-4`}>
+            <div className='w-full min-h-16 border-b border-zinc-700 flex items-center justify-center p-4'>
                 <PostCreator maxRows={8} placeholder="What do you want to share?" afterSubmit={id => setCreated(old => [id, ...old])}/>
             </div>
             {created.length > 0 && <ul className="w-full flex flex-col list-none divide-y divide-zinc-700 border-b border-zinc-700">

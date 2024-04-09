de5fae370da3eb7df355331c044cb18bde10ed1c
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-09 20:08:02 +0200

    Add custom select component

---
+++ front-end/src/components/form-utils/select/index.tsx
@@ -0,0 +1,16 @@
+import { FieldValues, Path, UseFormRegister } from "react-hook-form"
+
+interface SelectProps<T extends FieldValues> {
+    def?: string,
+    options: Map<string, string>,
+    register?: UseFormRegister<T>,
+    name?: Path<T>,
+}
+
+export default function Select<T extends FieldValues>({ options, register, name, def }: SelectProps<T>) {
+    return (
+        <select defaultValue={def} {...(register !== undefined && name !== undefined && register(name))} className="w-full bg-gray-700 px-1 py-0.5 rounded-lg text-gray-300 cursor-pointer !outline-none">
+            {Array.from(options).map((option, idx) => <option key={idx} value={option[0]}>{option[1]}</option>)}
+        </select>
+    )
+}

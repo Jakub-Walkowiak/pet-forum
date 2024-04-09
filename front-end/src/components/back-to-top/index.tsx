c7ad64285870483ceb41d1f649e285cc2f7aac56
Jakub_Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-09 20:05:57 +0200

    Fix incorrect appearance behavior and adjust treshold, add pointer cursor on hover

--- front-end/src/components/back-to-top/index.tsx
+++ front-end/src/components/back-to-top/index.tsx
@@ -7,9 +7,9 @@ export default function BackToTop() {
     const [show, setShow] = useState(false)
 
     useEffect(() => {
-        document.addEventListener('scroll', () => setShow(document.documentElement.scrollTop < 1500))
+        document.addEventListener('scroll', () => setShow(document.documentElement.scrollTop > 1200))
     }, [])
 
-    return <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className={`${show && 'scale-0'} hover:bg-emerald-500 duration-200 fixed bottom-6 right-6 bg-emerald-600 rounded-full w-14 h-14 text-5xl flex justify-center items-center`}>
+    return <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className={`${!show && 'scale-0'} cursor-pointer hover:bg-emerald-500 duration-200 fixed bottom-6 right-6 bg-emerald-600 rounded-full w-14 h-14 text-5xl flex justify-center items-center`}>
         <AiFillCaretUp className="relative bottom-1"/></div>
 }

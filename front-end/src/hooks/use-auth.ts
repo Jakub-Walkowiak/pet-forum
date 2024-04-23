aee1fd2f8ce443bf5bee98630cd4efd5aad5acfe
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-23 21:48:07 +0200

    Add auto-logout with auth cookie verification

--- front-end/src/hooks/use-auth.ts
+++ front-end/src/hooks/use-auth.ts
@@ -6,16 +6,21 @@ import { useInterval } from 'usehooks-ts'
 export default function useAuth() {
     const [authId, setAuthId] = useState<number>()
 
-    const verifyAuth = () => {
+    const verifyAuth = async () => {
         const tokenEncoded = Cookies.get('login_token')
-        if (tokenEncoded !== undefined) setAuthId(Number((<{id: string}>jwtDecode(tokenEncoded)).id))
+        if (tokenEncoded !== undefined) {
+            const decoded = Number((<{id: string}>jwtDecode(tokenEncoded)).id)
+            const response = await fetch(`http://localhost:3000/accounts/${decoded}`)
+            if (response.status !== 404) setAuthId(decoded)
+            else Cookies.remove('login_token', { path: '' })
+        }
         else setAuthId(undefined)
     }
 
     useEffect(() => {
         verifyAuth()
 
-        const handleRefresh =  () => verifyAuth()
+        const handleRefresh = () => verifyAuth()
     
         document.addEventListener('refreshauth', handleRefresh)
         return () => document.removeEventListener('refreshauth', handleRefresh)

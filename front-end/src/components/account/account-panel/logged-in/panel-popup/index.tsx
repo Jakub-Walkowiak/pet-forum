e4573a18f2c09b9f94d3a458a7d21e944f92010c
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-04-23 21:46:34 +0200

    Remove semicolons

--- front-end/src/components/account/account-panel/logged-in/panel-popup/index.tsx
+++ front-end/src/components/account/account-panel/logged-in/panel-popup/index.tsx
@@ -1,5 +1,5 @@
-import { routes } from '@/helpers/routes';
-import Cookies from 'js-cookie';
+import { routes } from '@/helpers/routes'
+import Cookies from 'js-cookie'
 
 interface PanelPopupProps {
     show: boolean,

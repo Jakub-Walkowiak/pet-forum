c90d61f692ad7c3ce9dface289990b64851bad0c
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-25 10:53:08 +0200

    Add /pets page

---
+++ front-end/src/app/pets/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import PetFeed from '@/components/content/dynamic-feeds/pet-feed'
+import PetSearchPanel from '@/components/forms/utils/feed-search-panels/pet-search-panel'
+import { PetFetchValidator } from '@/helpers/fetch-options/pet-fetch-options'
+import { useSearchParams } from 'next/navigation'
+import { Suspense, useEffect, useState } from 'react'
+
+function PageBody() {
+    const params = useSearchParams()
+    const [options, setOptions] = useState(PetFetchValidator.parse(Object.fromEntries(params.entries())))
+
+    useEffect(() => setOptions(PetFetchValidator.parse(Object.fromEntries(params.entries()))), [params])
+
+    return (
+        <>
+            <div className='w-full p-2 border-b border-zinc-700'>
+                <PetSearchPanel onSave={options => setOptions(options)} defaults={options}/>
+            </div>
+            <PetFeed options={options}/>
+        </>
+    )
+}
+
+export default function Page() {
+    return (
+        <Suspense>
+            <PageBody/>
+        </Suspense>
+    )
+}

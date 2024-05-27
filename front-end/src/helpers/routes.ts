c39b1e9456026719081d062f1fc8faf20d1c1a0d
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-27 10:48:09 +0200

    Add generic search page

--- front-end/src/helpers/routes.ts
+++ front-end/src/helpers/routes.ts
@@ -9,4 +9,5 @@ export const routes: Array<Route> = [
     { name: 'Home', path: '/', icon: '/placeholder-icon.svg', navbar: true },
     { name: 'Account', path: '/account', icon: '/placeholder-icon.svg', navbar: false },
     { name: 'Pets', path: '/pets/manage/', icon: '/placeholder-icon.svg', navbar: true },
+    { name: 'Search', path: '/search', icon: '/placeholder-icon.svg', navbar: true },
 ]

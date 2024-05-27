cdaf3ab0b342ec2635a534c4d570abb44ded6b5d
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-27 17:50:42 +0200

    Prevent dismissal of floating window when interacting with it

--- front-end/src/helpers/show-floating-element.tsx
+++ front-end/src/helpers/show-floating-element.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
 import { createRoot } from 'react-dom/client'
+import stopEvent from './stop-event'
 
 export default function showFloatingElement(element: ReactElement, x: number, y: number) {
     const rootElement = document.createElement('div')
@@ -8,6 +9,9 @@ export default function showFloatingElement(element: ReactElement, x: number, y:
     rootElement.style.position = 'fixed'
     rootElement.style.left = `${x}px`
     rootElement.style.top = `${y}px`
+
+    rootElement.addEventListener('click', stopEvent)
+    rootElement.addEventListener('scroll', stopEvent)
 
     document.body.append(rootElement)
 

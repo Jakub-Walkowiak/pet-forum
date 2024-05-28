6b2def83e5231b0149d888685aa27faeeefbf5ba
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-28 13:58:36 +0200

    Add onClickReplacement prop to pet pfp

--- front-end/src/components/images/profile-picture/pet/index.tsx
+++ front-end/src/components/images/profile-picture/pet/index.tsx
@@ -4,9 +4,10 @@ import ProfilePicture from ".."
 interface PetProfilePictureProps {
     id: number,
     sizeOverride?: number,
+    onClickReplacement?: () => void,
 }
 
-export default function PetProfilePicture({ id, sizeOverride }: PetProfilePictureProps) {
+export default function PetProfilePicture({ id, sizeOverride, onClickReplacement }: PetProfilePictureProps) {
     const data = usePet(id)
-    return <ProfilePicture redirectDestination={`/pets/${id}`} pictureId={data?.profilePictureId} sizeOverride={sizeOverride}/>
+    return <ProfilePicture onClick={onClickReplacement} redirectDestination={onClickReplacement ? '' : `/pets/${id}`} pictureId={data?.profilePictureId} sizeOverride={sizeOverride}/>
 }

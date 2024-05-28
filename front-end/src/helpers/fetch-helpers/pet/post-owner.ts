f8fedcef555209ea1b6a77a9c8ec6f1b180c1f9e
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-28 13:40:13 +0200

    Fix incorrect name of body param

--- front-end/src/helpers/fetch-helpers/pet/post-owner.ts
+++ front-end/src/helpers/fetch-helpers/pet/post-owner.ts
@@ -7,7 +7,7 @@ export default async function postOwner(account: number, pet: number, onSuccess?
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ id: account }),
+        body: JSON.stringify({ user: account }),
         credentials: 'include',
     })
 

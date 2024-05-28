25f345cd1b70ad895ccfc737c8f2aa92b2cedc72
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-28 17:59:35 +0200

    Change width of max size navigation menu

--- front-end/src/components/account/account-panel/full/logged-in/panel-popup/index.tsx
+++ front-end/src/components/account/account-panel/full/logged-in/panel-popup/index.tsx
@@ -15,7 +15,7 @@ export default function PanelPopup({ show }: PanelPopupProps) {
     }
 
     return (
-        <div className={`-z-10 duration-200 h-full cursor-auto absolute p-1 flex flex-col w-full bg-gray-800/30 rounded-t-lg text-xl ${show ? '-translate-y-full opacity-1 pointer-events-auto' : 'translate-y-0 opacity-0 pointer-events-none'}`}
+        <div className={`-z-10 duration-200 h-full cursor-auto absolute left-0 top-0 p-1 flex flex-col w-full bg-gray-800/30 rounded-t-lg text-xl ${show ? '-translate-y-full opacity-1 pointer-events-auto' : 'translate-y-0 opacity-0 pointer-events-none'}`}
             onClick={e => e.stopPropagation()}>
             <div className='duration-200 w-full cursor-pointer hover:bg-gray-800 p-1 rounded-lg flex flex-1'>
                 <a href={accountRoute?.path} className='flex w-full h-full items-center'>

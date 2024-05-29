29fc3888d75db517b9315de8cfe504cdf8f18c17
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-29 18:35:51 +0200

    Fix incorrect error handling in account follows router

--- back-end/src/routes/accounts/follows.ts
+++ back-end/src/routes/accounts/follows.ts
@@ -11,9 +11,9 @@ FollowRouter.post('/', authMandatory, (req, res, next) => {
     pool.query(sql, [req.params.id, req.body.id])
         .then(() => res.status(204).send())
         .catch(err => {
-            if (err.code === 23505) res.status(409).json(CONFLICT)
-            else if (err.code === 23503) res.status(404).json(RESOURCE_NOT_FOUND)
-            else if (err.code === 23514) res.status(403).json(FORBIDDEN)
+            if (err.code === '23505') res.status(409).json(CONFLICT)
+            else if (err.code === '23503') res.status(404).json(RESOURCE_NOT_FOUND)
+            else if (err.code === '23514') res.status(403).json(FORBIDDEN)
             else next(err)
         })
 })
@@ -23,10 +23,7 @@ FollowRouter.delete('/', authMandatory, (req, res, next) => {
 
     pool.query(sql, [req.params.id, req.body.id])
         .then(() => res.status(204).send())
-        .catch(err => {
-            if (err.code = 23503) res.status(404).json(RESOURCE_NOT_FOUND)
-            else next(err)
-        })
+        .catch(err => next(err))
 })
 
 export { FollowRouter }

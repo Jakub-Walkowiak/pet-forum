7107e90e488e4decde23fdf83dcdcaef9b032e66
Jakub-Walkowiak <walkowiakgames.jw@gmail.com>
2024-05-30 13:44:02 +0200

    Add test for form image

---
+++ front-end/src/components/images/form-image/FormImage.test.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react"
+import { act } from "react-dom/test-utils"
+import FormImage from "."
+
+describe('Form image', () => {
+    const props = {
+        src: '/',
+        remove: () => {},
+    }
+
+    it('should display an image', () => {
+        const { getByAltText } = render(<FormImage {...props}/>)
+        expect(getByAltText('Uploaded image')).toBeTruthy()
+    })
+
+    it('should go fullscreen when clicked', () => {
+        const { getByAltText } = render(<FormImage {...props}/>)
+        act(() => getByAltText('Uploaded image').click())
+        expect(getByAltText('Fullscreen image')).toBeTruthy()
+    })
+})
